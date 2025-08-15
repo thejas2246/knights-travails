3fe275ec742b75224a72e411b95eaaf4f77aeff9
thejas <thejas2246@gmail.com>
2025-08-15 19:19:15 +0530

    Create a array to track visited nodes

---
+++ knight-travails.js
@@ -0,0 +1,9 @@
+function knightMoves(originalPosition,targetPosition){
+
+    let columns = 8;
+    let rows = 8;
+    let visited = Array.from({length:rows},()=>new Array(columns).fill(false));
+}
+
+knightMoves([0,0],[1,2]);
+
