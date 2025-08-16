6a0dbacb6a06a9168bd98c4ee6ab1dc7d017b80c
thejas <thejas2246@gmail.com>
2025-08-16 11:35:33 +0530

    Check if the original and target position is same

--- knight-travails.js
+++ knight-travails.js
@@ -1,14 +1,35 @@
+function equalsArray(a,b){
+    if(a===b) return true;
+    if(a==null||b==null) return false;
+    if(a.length!==b.length) return false;
+
+    for(let i =0;i<a.length;i++){
+        if(a[i]!==b[i]) return false
+    }
+    return true
+}
+
 function knightMoves(originalPosition,targetPosition){
+    if(equalsArray(originalPosition,targetPosition)){
+        throw new Error("same original and target position")
+    }
 
     let columns = 8;
     let rows = 8;
+if(originalPosition[0]<0||originalPosition[0]>=rows||originalPosition[1]<0||originalPosition[1]>=columns){
+    throw new Error("out of range");
+}
+if(targetPosition[0]<0||targetPosition[0]>=rows||targetPosition[1]<0||targetPosition[1]>=columns){
+    throw new Error("out of range");
+}
+    
     let visited = Array.from({length:rows},()=>new Array(columns).fill(false));
     let queue = [];
     let moves = [[2,-1],[2,1],[1,-2],[-1,-2],[1,2],[-1,2],[-2,1],[-2,-1]];
     let parent = Array.from({length:rows},()=>new Array(columns).fill(null));
     queue.push(originalPosition);
     visited[originalPosition[0]][originalPosition[1]] = true;
-    didFound = false;
+    let didFound = false;
     while(queue.length!==0){
     
         let currentNode = queue.shift();
@@ -50,5 +71,5 @@ function knightMoves(originalPosition,targetPosition){
 
 }
 
-knightMoves([0,0],[7,7]);
+knightMoves([0,0],[0,7]);
 
