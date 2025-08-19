8e3d3ab0ce91cb5ec4d63084186871b3a5237c05
Thejas <thejas2246@gmail.com>
2025-08-19 13:45:23 +0530

    Create a chess board

--- ui.js
+++ ui.js
@@ -6,6 +6,13 @@ function createBoard(){
     for(let i=0;i<8;i++){
         for(let j=0;j<8;j++){
             const block = document.createElement("div");
+            if((i+j)%2===0){
+                block.setAttribute("class","white");
+            }
+            else{
+                block.setAttribute("class","black")
+            }
+            block.setAttribute('data-index',`${i}${j}`)
             boardContainer.appendChild(block);
         }
     }
