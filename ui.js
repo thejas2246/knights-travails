ca45077ee320ede90c0617c6f162b99f5e59047c
Thejas <thejas2246@gmail.com>
2025-08-20 09:29:37 +0530

    Set target position on board

--- ui.js
+++ ui.js
@@ -3,7 +3,7 @@ import { knightMoves } from "./knight-travails.js";
 
 const boardContainer = document.querySelector(".board-container");
 
-let knight = new Knight();
+const knight = new Knight();
 
 function createBoard(){
     
@@ -21,16 +21,37 @@ function createBoard(){
             boardContainer.appendChild(block);
         }
     }
+    let targetPosition = knight.getTargetPostition();
+    setKnightImageOnBoard(knight.initialPosition,targetPosition);
 }
 
 function addEventLisnerToBlock(block){
     block.addEventListener("click",()=>{
+        setTargetOpacity(knight.getTargetPostition(),1)
         let val = block.getAttribute(["data-index"])
         let index = val.split('');
-
-        showPathOnBoard([0,0],[Number(index[0]),Number(index[1])])
+        index[0] = Number(index[0]);
+        index[1] = Number(index[1]);
+        knight.setTargetPosition(index)
+       setTargetOpacity(knight.getTargetPostition())
     })
 }
+
+
+
+function setTargetOpacity(target,opacity = 0.7){
+     target =  document.querySelector(`[data-index = '${target[0]}${target[1]}']`)
+     target.style.opacity = opacity
+}
+
+function setKnightImageOnBoard(initial,target){
+    let starPosition = document.querySelector(`[data-index = '${initial[0]}${initial[1]}']`)
+     const image = document.createElement("img");
+     image.src = "./image/knight.png"
+     starPosition.appendChild(image)
+    setTargetOpacity(target)
+}
+
 
 let delay = 1000;
 function showPathOnBoard(start,end){
